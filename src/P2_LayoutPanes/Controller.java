d4c3e2ffa2564b6414fa8e50ef95e5013a5d3f65
Hai Pham <ngochai285nd@gmail.com>
2022-12-29 22:59:20 +0700

    update P2_LayoutPanes

---
+++ src/P2_LayoutPanes/Controller.java
@@ -0,0 +1,4 @@
+package P2_LayoutPanes;
+
+public class Controller {
+}
---
+++ src/P2_LayoutPanes/Main.java
@@ -0,0 +1,23 @@
+package P2_LayoutPanes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Layout Panes");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
